1c3a2018bf1e82c053ad420939d0acf5761e0b1a
Gonzalo Guerrero <contacto@gonzaloelias.me>
2020-09-14 06:42:40 -0300

    main button added

--- src/burger.js
+++ src/burger.js
@@ -1,18 +1,15 @@
+const showMenu = (burgerId,navId) => {
+     const burger = document.getElementById(burgerId),
+        nav = document.getElementById(navId)
+    if(burger && nav) {
+        burger.addEventListener('click', () => {
+            nav.classList.toggle('show')
+        })
+    }
+}
 
+showMenu('main-menu-burger', 'main-nav')
 
-document.querySelector('.main-menu-burger').addEventListener('click', ()=> {
+/* document.querySelector('.main-menu-burger').addEventListener('click', ()=> {
     document.querySelector('.main-nav').classList.toggle('show');
-});
-
-
-// const showMenu = (burgerId,navId) => {
-//      const burger = document.getElementById(burgerId),
-//         nav = document.getElementById(navId)
-//     if(burger && nav) {
-//         burger.addEventListener('click', () => {
-//             nav.classList.toggle('show')
-//         })
-//     }
-// }
-
-// showMenu('main-menu-burger', 'main-nav')
+}); */
