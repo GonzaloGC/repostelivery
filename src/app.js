a6caac653425cf1ff5c34c5a4f4026ff79eb334d
Gonzalo Guerrero <contacto@gonzaloelias.me>
2020-09-14 22:10:45 -0300

    button, images and cards modificated

--- src/app.js
+++ src/app.js
@@ -2,17 +2,14 @@
 import "./burger";
 import "./map";
 import "./styles/main.scss";
-import "./static/img/logo-bl.png";
 import "./static/img/estrellas.png";
+import "./static/img/galletas1.jpg";
 import "./static/img/search.svg";
-import "./static/img/pastel3.jpg";
-import "./static/img/torta1.png";
-import "./static/img/torta11.png";
-import "./static/img/torta2.png";
-import "./static/img/torta3.png";
-import "./static/img/torta4.png";
-import "./static/img/torta5.png";
-import "./static/img/torta6.png";
+import "./static/img/receta1.jpg";
+import "./static/img/receta2.jpg";
+import "./static/img/receta3.jpg";
+import "./static/img/receta4.jpg";
+
 import "./static/img/facebook.svg";
 import "./static/img/instagram.svg";
 import "./static/img/twitter.svg";
