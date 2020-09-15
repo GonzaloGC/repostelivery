7e095b1abfdd5923b76333366d7891d3706076ce
Gonzalo Guerrero <contacto@gonzaloelias.me>
2020-09-15 03:29:09 -0300

    starting with css grid in cards

--- src/map.js
+++ src/map.js
@@ -4,8 +4,20 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: -33.6220656, lng: -70.8702453 },
     zoom: 8
-  });
+  }),
+  map = new google.maps.Map(document.getElementById("map"),{
+        center: { lat: -33.6220656, lng: -70.8702453 },
+        zoom: 10,
+        // mapTypeID: google.maps.MapTypeId.ROADMAP,
+    }),
+    marker = new google.maps.Marker(document.getElementById("map"),{
+        position: coords,
+        map,
+        title: 'Repostelivery'
+    });
 }
+
+initMap();
 
 /* marker = new google.maps.Marker({
   position: { lat: -33.6220656, lng: -70.8702453 },
