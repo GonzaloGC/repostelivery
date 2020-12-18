2d4c6c3a6cb5548994656c042d8aa8914d966c61
Gonzalo Guerrero <contacto@gonzaloelias.me>
2020-12-17 23:50:01 -0300

    added image ´receta5´

--- src/app.js
+++ src/app.js
@@ -5,10 +5,12 @@ import "./styles/main.scss";
 import "./static/img/estrellas.png";
 import "./static/img/galletas1.jpg";
 import "./static/img/search.svg";
+// import "./static/img/torta.jpg";
 import "./static/img/receta1.jpg";
 import "./static/img/receta2.jpg";
 import "./static/img/receta3.jpg";
 import "./static/img/receta4.jpg";
+import "./static/img/receta5.jpg";
 
 import "./static/img/facebook.svg";
 import "./static/img/instagram.svg";
