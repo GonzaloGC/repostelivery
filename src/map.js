e0da4c0def673a1b17c9e7ca1aa97456907188d9
Gonzalo Guerrero <contacto@gonzaloelias.me>
2021-01-11 23:49:23 -0300

    Added scroll item in buttom 'Ver productos' and z-index in map

--- src/map.js
+++ src/map.js
@@ -8,7 +8,7 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"),{
         center: { lat: -33.6220656, lng: -70.8702453 },
         zoom: 10,
-        // mapTypeID: google.maps.MapTypeId.ROADMAP,
+        
     }),
     marker = new google.maps.Marker(document.getElementById("map"),{
         position: coords,
@@ -18,21 +18,3 @@ function initMap() {
 }
 
 initMap();
-
-/* marker = new google.maps.Marker({
-  position: { lat: -33.6220656, lng: -70.8702453 },
-  map,
-  title: 'Repostelivery'
-}) */
-// let marker; = new google.maps.Marker({
-//     position: coords,
-//     map,
-//     title: 'Repostelivery'
-// });
-
-// let map = new google.maps.Map(document.getElementById('map'),{
-//     center: { lat: -33.6220656, lng: -70.8702453 },
-//     zoom: 10,
-//     // mapTypeID: google.maps.MapTypeId.ROADMAP,
-// });
-
